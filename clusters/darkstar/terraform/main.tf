359e8052666ceb43ebabb9174d594cae8374f848
bashfulrobot <dustin@bashfulrobot.com>
2024-01-29 17:03:46 -0800

    Chore: add darkstar terraform files Changes to be committed: 	new file:   clusters/darkstar/terraform/.terraform.lock.hcl 	new file:   clusters/darkstar/terraform/.terraform.tfstate.lock.info 	new file:   clusters/darkstar/terraform/main.tf 	new file:   clusters/darkstar/terraform/providers.tf 	new file:   clusters/darkstar/terraform/terraform.tf 	new file:   clusters/darkstar/terraform/terraform.tfstate 	new file:   clusters/darkstar/terraform/terraform.tfvars 	new file:   clusters/darkstar/terraform/variables.tf

---
+++ clusters/darkstar/terraform/main.tf
@@ -0,0 +1,77 @@
+
+module "network" {
+  source = "github.com/bashfulrobot/libvirt-module-network"
+
+  #### Network Variables
+  kvm_subnet        = var.kvm_subnet
+  kvm_subnet_prefix = var.kvm_subnet_prefix
+  network_name      = var.cluster_name
+
+}
+
+module "cp" {
+  source = "github.com/bashfulrobot/libvirt-module-kubeadm-cp"
+
+  #### User Variables
+  admin_name             = var.admin_name
+  admin_password_hash    = var.admin_password_hash
+  path_to_ssh_public_key = var.path_to_ssh_public_key
+
+  #### Cluster/Host Variables
+  cluster_name = var.cluster_name
+  host_prefix  = var.host_prefix
+  host_suffix  = var.vm_host_suffix
+
+  #### Network Variables
+  network_domain    = module.network.network_domain
+  network_id        = module.network.network_id
+  kvm_subnet_prefix = var.kvm_subnet_prefix
+
+  #### VM Variables
+  vm_vcpu         = var.vm_vcpu
+  vm_memory       = var.vm_memory
+  vm_os_disk_size = var.vm_os_disk_size
+  vm_count        = var.vm_count
+
+  #### Libvirt Variables
+  image_url = var.image_url
+  autostart = var.autostart
+
+}
+
+module "worker" {
+  source = "github.com/bashfulrobot/libvirt-module-kubeadm-worker"
+
+  #### User Variables
+  admin_name             = var.admin_name
+  admin_password_hash    = var.admin_password_hash
+  path_to_ssh_public_key = var.path_to_ssh_public_key
+
+  #### Cluster/Host Variables
+  cluster_name = var.cluster_name
+  host_prefix  = var.host_prefix
+  host_suffix  = var.datavm_host_suffix
+
+  #### Network Variables
+  network_domain    = module.network.network_domain
+  network_id        = module.network.network_id
+  kvm_subnet_prefix = var.kvm_subnet_prefix
+
+  #### Datavm Variables
+  vm_vcpu           = var.datavm_vcpu
+  vm_memory         = var.datavm_memory
+  vm_os_disk_size   = var.datavm_os_disk_size
+  vm_data_disk_size = var.datavm_disk_size
+  vm_count          = var.datavm_count
+
+  ####  Libvirt Variables
+  image_url = var.image_url
+  autostart = var.autostart
+
+}
+
+#### output "network_id" {
+####   description = "ID of the created network"
+####   #### returned by my network module
+####   value = module.network.network_id
+#### }
